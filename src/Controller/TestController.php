7da5aa769dd0b178ad313b5d15575f7b68141103
tarlepp <tarmo.leppanen@protacon.com>
2018-11-29 17:52:25 +0200

    Example how to override GET request options

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -16,7 +16,13 @@ class TestController extends AbstractController
      */
     public function index(TestClient $client)
     {
-        $response = $client->get('');
+        $response = $client->get(''); // Default one
+
+        // 1. solution: override options on your `get` method call
+        //$response = $client->get('', ['http_errors' => false]);
+
+        // 2. solution: use custom method on that `TestClient` service
+        //$response = $client->customGet('');
 
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
