c7ed64141ad56e183951ac972f8ab62c2a358426
tarlepp <tarmo.leppanen@protacon.com>
2018-11-29 20:49:49 +0200

    service example

--- src/Service/TestService.php
+++ src/Service/TestService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Client\TestClient;
+use Psr\Http\Message\UriInterface;
 
 class TestService
 {
@@ -11,17 +12,22 @@ class TestService
     /**
      * TestService constructor.
      *
-     * @param \App\Client\TestClient $client
+     * @param TestClient $client
      */
     public function __construct(TestClient $client)
     {
         $this->client = $client;
     }
 
-    public function checkThatSomethingIsOk(): string
+    /**
+     * @param string|UriInterface $uri
+     *
+     * @return string
+     */
+    public function checkThatSomethingIsOk($uri): string
     {
         // I'm making request that is not going to return HTTP 200 status code
-        $response = $this->client->get('url-to-get-some-data', ['http_errors' => false]);
+        $response = $this->client->customRequest($uri);
 
         // Do something with that response and determine if all is ok or not
 
